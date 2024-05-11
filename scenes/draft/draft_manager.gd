2e933ee46b7ccf241f4f637b09d3cc4f62eae331
sterco-raro <unarmedpile@gmail.com>
2024-05-11 09:47:13 +0200

    Working draft spawn with random position and rotation

--- scenes/draft/draft_manager.gd
+++ scenes/draft/draft_manager.gd
@@ -22,6 +22,8 @@ var _card_prefabs : Array[Array] = [
 ]
 const CARD_PREFABS_SIZE: int = 5
 
+var _current_draft: Array[Card]
+
 func _ready():
 	assert(DECK != null, "DECK is null")
 	_generate_new_draft()
@@ -45,9 +47,7 @@ func _shuffle_card_values():
 				done = true
 				used_values.append(value)
 				_card_prefabs[i][1] = value
-				print_debug("_shuffle_card_values: current value is %d, assigned value is %d" % [ value, _card_prefabs[i][1] ])
 		done = false
-	print_debug(_card_prefabs)
 
 func _empty_old_hand():
 	var count: int = _cards_container.get_child_count()
@@ -57,17 +57,45 @@ func _empty_old_hand():
 func _pick_random_hand():
 	var type: int
 	var card: Card
-	var offset: int
 	var draft_size: int = randi_range(MIN_DRAFT_SIZE, MAX_DRAFT_SIZE)
 	for i: int in draft_size:
 		# Card type
+		# TODO add weighted chances: pick a value in 0-100 and assign ranges to the array indexes 0-5
 		type = randi() % CARD_PREFABS_SIZE
 		# Card data
 		card = _card_prefabs[type][2].instantiate()
 		card.index = i
 		card.value = _card_prefabs[type][1]
-		print_debug(_card_prefabs[type][1], card.value)
 		# Node position
 		_cards_container.add_child(card)
-		offset = _cards_container.position.x / 2 + (64 + 32) * (i + 1)
-		card.position += Vector2(offset, 0)
+		_randomize_position_and_rotation(card)
+		_current_draft.append(card)
+
+func _randomize_position_and_rotation(card: Node2D):
+	var border = 32
+	var sprite_rect: Rect2 = ($Drawer/Sprite2D as Sprite2D).get_rect()
+
+	# rect position is top-left usually
+	var min_x = sprite_rect.position.x + border
+	var min_y = sprite_rect.position.y + border
+	# rect size is the width and height from position
+	var max_x = sprite_rect.size.x - border
+	var max_y = sprite_rect.size.y - border
+
+	var done = false
+	var overlap = false
+	var point: Vector2
+	while (!done):
+		point = Vector2(randi_range(min_x, max_x), randi_range(min_y, max_y))
+
+		if !sprite_rect.has_point(point): continue
+
+		card.position = point
+		card.rotate(randf_range(0, 2 * PI))
+
+		var rect: Rect2 = Rect2(point, Vector2(border, border))
+		for c: Card in _current_draft:
+			if rect.has_point(c.position):
+				overlap = true
+		if !overlap:
+			done = true
